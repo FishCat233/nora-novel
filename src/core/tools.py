d301fc28ed669e2ef02851f007d1fbd6ad36bfab
FishCat233 <50800434+FishCat233@users.noreply.github.com>
2026-02-28 16:35:39 +0800

    feat(tools): 实现了 ask_user 工具并重构了 view 和 agent 类

--- src/core/tools.py
+++ src/core/tools.py
@@ -1,56 +1,81 @@
-from typing import Iterable
-from openai.types.chat import ChatCompletionToolUnionParam
+from typing import Type, Union, Literal
+from pydantic import BaseModel, ConfigDict
 
-tools: Iterable[ChatCompletionToolUnionParam] = [
-    {
-        "type": "function",
-        "function": {
-            "name": "get_current_time",
-            "description": "Get the current time with format",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "time_format": {
-                        "type": "string",
-                        "description": 'The format of the time, e.g. Example: "%d/%m/%Y, %H:%M:%S" for "2026/02/28, 13:02:24"',
-                    }
-                },
-                "required": ["time_format"],
-                "additionalProperties": False,
-            },
-            "strict": True,
-        },
-    }
-    # {
-    #     "type": "function",
-    #     "function": {
-    #         "name": "ask_user_question",
-    #         "description": "Ask the user a clarification question before continuing",
-    #         "parameters": {
-    #             "type": "object",
-    #             "properties": {
-    #                 "question": {
-    #                     "type": "string",
-    #                     "description": "The question to ask the user",
-    #                 }
-    #             },
-    #             "required": ["question"],
-    #         },
-    #     },
-    # }
+
+class GetCurrentTimeParams(BaseModel):
+    time_format: str
+
+    model_config = ConfigDict(extra="forbid")
+
+
+class AskUserParams(BaseModel):
+    prompt: str
+
+    model_config = ConfigDict(extra="forbid")
+
+
+tool_registry = {}
+tool_meta = {}
+tools = []
+
+type ToolType = Union[
+    Literal["function"],  # 函数工具，如检查时间
+    Literal["human_callback"],  # 用户回调工具，如询问用户
 ]
+
+
+@staticmethod
+def register_tool(schema_model: Type[BaseModel], tool_type: ToolType = "function"):
+    """
+    注册工具到仓库
+    Args:
+        schema_model: 工具的参数 BaseModel
+        tool_type: 工具类型，目前支持 function 和 human_callback
+
+    Returns: wrapper
+    """
+
+    def wrapper(func):
+        tool_registry[func.__name__] = func
+        tool_meta[func.__name__] = {
+            "type": tool_type,
+        }
+
+        tools.append(
+            {
+                "type": "function",
+                "function": {
+                    "name": func.__name__,
+                    "description": func.__doc__,
+                    "parameters": schema_model.model_json_schema(),
+                },
+            }
+        )
+
+        return func
+
+    return wrapper
 
 
 class Tool:
     @staticmethod
     def dispatch(name: str, arguments: dict):
-        if name == "get_current_time":
-            return Tool.get_current_time(arguments["time_format"])
+        if name in tool_registry:
+            return tool_registry[name](**arguments)
 
         raise ValueError(f"Unknown tool: {name}")
 
     @staticmethod
-    def get_current_time(time_format: str):
+    @register_tool(GetCurrentTimeParams)
+    def get_current_time(time_format: str) -> str:
+        """
+        获取当前时间
+        Args:
+            time_format: 时间格式，例如 "%Y-%m-%d %H:%M:%S" 对应 "2026-02-28 14:36:02"。
+                更多格式请参照 python strftime 函数
+
+        Returns: 当前时间 strftime 格式化后的字符串
+        """
         import datetime
 
         try:
@@ -58,3 +83,15 @@ class Tool:
             return now.strftime(time_format)
         except ValueError:
             return "Invalid time format"
+
+    @staticmethod
+    @register_tool(AskUserParams, tool_type="human_callback")
+    def ask_user(prompt: str) -> str:
+        """
+        向用户提问
+        Args:
+            prompt: 提问内容
+
+        Returns: 用户回答
+        """
+        pass
--- src/view/__init__.py
+++ src/view/__init__.py
@@ -1,6 +1,10 @@
+import json
 from abc import ABC, abstractmethod
 from openai import OpenAI
 import streamlit as st
+
+from src.core.core import NoraAgent
+from src.core.types import CustomMessage
 
 
 class IView(ABC):
@@ -11,7 +15,7 @@ class IView(ABC):
 
 class MainView(IView):
     def __init__(self):
-        self.agent = st.session_state.agent
+        self.agent: NoraAgent = st.session_state.agent
 
     def display(self):
         self.initialize()
@@ -29,16 +33,73 @@ class MainView(IView):
             if message.role == "user":
                 with st.chat_message("user"):
                     st.markdown(message.content)
-            else:
+            elif message.role == "assistant":
+                # 如果是 tool_calls 则显示 tool_calls
                 with st.chat_message("assistant"):
                     st.markdown(message.content)
 
+                    if message.tool_calls:
+                        for tool_call in message.tool_calls:
+                            st.markdown(
+                                f"*调用工具: {tool_call.function.name}, 参数: {tool_call.function.arguments}*"
+                            )
+            elif message.role == "tool":
+                # 显示工具调用结果
+                with st.chat_message("user"):
+                    st.markdown(f"*调用结果: {message.content}*")
+
     def chat_input(self):
         """输入框"""
-        if prompt := st.chat_input("输入你的消息..."):
-            self.agent.chat(
-                prompt
-            )  # TODO: 基于返回值可以做增量更新显示，不过会省略中间的工具调用过程. 后面再弄把.
+        # 如果在等待工具回复
+        if st.session_state.pending_tool_call:
+            self.handle_pending_tool_call()
 
-            # 刷新聊天记录
-            self.load_messages()
+        # 如果在等待用户会话
+        if prompt := st.chat_input("输入你的消息..."):
+            result = self.agent.step(prompt)
+
+            if result == "finished":
+                pass
+
+            # agent 调用工具
+            if isinstance(result, list):
+                for tool_call in result:
+                    st.session_state.pending_tool_call = tool_call
+
+            st.rerun()
+
+    def handle_pending_tool_call(self):
+        tool_call = st.session_state.pending_tool_call
+
+        name = st.session_state.pending_tool_call.function.name
+        args = json.loads(st.session_state.pending_tool_call.function.arguments)
+
+        # 如果需要 ui 则先调用 ui
+        if name == "ask_user":
+            prompt = args["prompt"]
+            answer = st.text_input(prompt)
+            if answer:
+                self.agent.messages.append(
+                    CustomMessage(
+                        role="tool",
+                        content=answer,
+                        tool_call_id=tool_call.id,
+                        tool_call_name=tool_call.function.name,
+                    )
+                )
+                st.session_state.pending_tool_call = None
+                self.agent.step()
+                st.rerun()
+        else:
+            # 调用工具
+            self.agent.use_tool_call(tool_call)
+            st.session_state.pending_tool_call = None
+            self.agent.step()
+            st.rerun()
+
+    @staticmethod
+    def human_callback(tool_name: str, args):
+        if tool_name == "ask_user":
+            return st.text_input(args["prompt"])
+        else:
+            raise NotImplementedError(f"Unknown tool: {tool_name}")
